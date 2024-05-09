4fc8e6b42669a9c05d3d845a21cbc1dc9d29cffa
Hubert Damaszko <wiazhub@gmail.com>
2024-05-09 13:29:14 +0200

    Queries with Dtos for CQRS implemented

---
+++ src/AlledrogO.Post/AlledrogO.Post.Application/Queries/SearchPosts.cs
@@ -0,0 +1,7 @@
+using AlledrogO.Post.Application.DTOs;
+using AlledrogO.Post.Application.DTOs.External;
+using AlledrogO.Shared.Queries;
+
+namespace AlledrogO.Post.Application.Queries;
+
+public record SearchPosts(PostSearchWithTagsDto Search) : IQuery<IEnumerable<PostDto>>;
