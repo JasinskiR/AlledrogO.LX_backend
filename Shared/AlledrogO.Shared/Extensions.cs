d4efed54900f5b7592dee5b13924f4258c7357e4
HubertDamaszko <259300@student.pwr.edu.pl>
2024-06-05 16:20:42 +0200

    Authentication to Swagger

--- Shared/AlledrogO.Shared/Extensions.cs
+++ Shared/AlledrogO.Shared/Extensions.cs
@@ -31,6 +31,35 @@ public static class Extensions
                Title = "Alledrogo API",
                Version = "v1"
            });
+           swagger.AddSecurityDefinition("Bearer",
+               new OpenApiSecurityScheme
+               {
+                   Description =
+                       "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 12345abcdef\"",
+                   Name = "Authorization",
+                   In = ParameterLocation.Header,
+                   Type = SecuritySchemeType.ApiKey,
+                   Scheme = "Bearer"
+               });
+
+           swagger.AddSecurityRequirement(new OpenApiSecurityRequirement()
+           {
+               {
+                   new OpenApiSecurityScheme
+                   {
+                       Reference = new OpenApiReference
+                       {
+                           Type = ReferenceType.SecurityScheme,
+                           Id = "Bearer"
+                       },
+                       Scheme = "oauth2",
+                       Name = "Bearer",
+                       In = ParameterLocation.Header,
+
+                   },
+                   new List<string>()
+               }
+           });
        });
        services.AddAuthorizationBuilder();
        services.AddAuthentication();
